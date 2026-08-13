f3d4ea26b58b8276fe8c063fe743e97b88b54344
giovannavitoriarezende-ctrl <giovanna.vitoria.rezende@escola.pr.gov.br>
2026-08-13 08:47:54 -0300

    ok

---
+++ script.js
@@ -0,0 +1,11 @@
+//Botão curtidas
+document.addEventListener("DOMcontentLoaed" , () =>{
+    const lik8tn = ocument. queySelector (".left.action .action.btn:firs.chil");
+    if (!lik8yn) return;
+    const like = lik8tn. querySelector9("syg");
+
+    //localiza o contador
+
+    let textNode = Array
+})
+
