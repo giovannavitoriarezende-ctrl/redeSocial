7fd4fd29baaf0aa773108d8c3e86ac63aab3c7a2
giovannavitoriarezende-ctrl <giovanna.vitoria.rezende@escola.pr.gov.br>
2026-08-20 08:49:21 -0300

    ok

--- script.js
+++ script.js
@@ -6,6 +6,28 @@ document.addEventListener("DOMcontentLoaed" , () =>{
 
     //localiza o contador
 
-    let textNode = Array
+    let textNode = Array. from(like8th.childNodes) .find(node) => node.nodeType
+
+    === Node. TEXT_NODE && node. textContent.trin() !== ""
+) ;
+
+let cont = 0;
+
+if(textNode){
+
+    textNode. textContent = `0` ;
+}
+
+function applyLikeSyle(){
+    likeSvg.style.fill = "red";
+    likeSvg.style.stroke = "red"
+    likeSvg.style.color = red
+
+likeSvg.style.transform = "scale(1.3)";
+setTimeout(() => (likeSvg.style.transform = "scale(1)" {
+    
+}, timeout);
+
+
 })
 
